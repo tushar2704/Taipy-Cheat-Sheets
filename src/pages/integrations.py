fd7853199d9db760549d44e38d23a917326926a7
tushar27 <tushar.27041994@gmail.com>
2024-05-04 06:28:17 +0530

    integration

--- src/pages/integrations.py
+++ src/pages/integrations.py
@@ -2,12 +2,24 @@
 ##Taipy Cheat Sheets [https://github.com/tushar2704/Taipy-Cheat-Sheets] (https://github.com/tushar2704/Taipy-Cheat-Sheets)
 #######################################################################################################
 #Importing dependecies
-#######################################################################################################
-from taipy.gui import Markdown, Gui, Dropdown
+#######################################################################################################from taipy.gui import Markdown, Gui, Dropdown
 
 page = """
-# Getting Started
+# Integrations
 
-Welcome to the Taipy Cheatsheets! This guide will help you get up and running with Taipy quickly.
+Taipy integrates with various databases, machine learning libraries, and cloud platforms.
 
-<|Dropdown|label=Installation Code|>"""
+<|Dropdown|label=Database Integration Code|>
+```python
+from taipy.core.data import PostgreSQLDataNode
+
+data_node = PostgreSQLDataNode(
+    id="my_data",
+    host="localhost",
+    port=5432,
+    database="my_db",
+    username="user",
+    password="password",
+    table="my_table"
+)
+"""
