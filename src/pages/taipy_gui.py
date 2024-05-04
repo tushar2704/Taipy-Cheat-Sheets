7a27e903be72c8c2933ca2684d601ec93ddfe53e
tushar27 <tushar.27041994@gmail.com>
2024-05-04 05:46:04 +0530

    pages demo updates

--- src/pages/taipy_gui.py
+++ src/pages/taipy_gui.py
@@ -3,3 +3,31 @@
 #######################################################################################################
 #Importing dependecies
 #######################################################################################################
+from taipy.gui import Markdown, Gui, Dropdown
+
+page = """
+# Taipy GUI
+
+Taipy GUI allows you to create interactive web interfaces for your Taipy applications.
+
+<|Dropdown|label=Layout Code|>
+"""
+from taipy.gui import Gui, Markdown, Button
+
+page = """
+<|layout|columns=2 1|
+<|
+# Left Column
+|>
+
+<|
+# Right Column
+<|Button|label=Click Me|on_action=button_clicked|>
+|>
+|>
+"""
+
+def button_clicked(state):
+    print("Button clicked!")
+
+Gui(page=Markdown(page)).run()
--- src/pages/taipy_rest.py
+++ src/pages/taipy_rest.py
@@ -3,3 +3,20 @@
 #######################################################################################################
 #Importing dependecies
 #######################################################################################################
+from taipy.gui import Markdown, Gui, Dropdown
+
+page = """
+# Taipy REST
+
+Taipy REST allows you to expose your Taipy scenarios as RESTful APIs.
+
+<|Dropdown|label=Endpoint Code|>
+""" 
+from taipy.rest import Rest, endpoint
+
+@endpoint(path="/hello", methods=["GET"])
+def hello():
+    return {"message": "Hello, World!"}
+
+rest = Rest()
+rest.run()
