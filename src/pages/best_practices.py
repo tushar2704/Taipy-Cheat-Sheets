e72554c5ddac8aab6d2307512eda0c4109c5873f
tushar27 <tushar.27041994@gmail.com>
2024-05-04 06:22:58 +0530

    pages updates

---
+++ src/pages/best_practices.py
@@ -0,0 +1,13 @@
+##© 2024 Tushar Aggarwal. All rights reserved.(https://tushar-aggarwal.com)
+##Taipy Cheat Sheets [https://github.com/tushar2704/Taipy-Cheat-Sheets] (https://github.com/tushar2704/Taipy-Cheat-Sheets)
+#######################################################################################################
+#Importing dependecies
+#######################################################################################################
+from taipy.gui import Markdown, Gui, Dropdown
+
+page = """
+# Getting Started
+
+Welcome to the Taipy Cheatsheets! This guide will help you get up and running with Taipy quickly.
+
+<|Dropdown|label=Installation Code|>"""
