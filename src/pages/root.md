4271e56bbf271e07625dc203bcda2eff858165b6
tushar27 <tushar.27041994@gmail.com>
2024-05-19 23:21:28 +0530

    pages structure test

---
+++ src/pages/root.md
@@ -0,0 +1,9 @@
+
+# [Taipy Cheat Sheets](https://github.com/tushar2704/Taipy-Cheat-Sheets)
+
+by [Tushar Aggarwal](https://www.linkedin.com/in/tusharaggarwalinseec/)
+
+Welcome! This guide will help you get up and running with Taipy quickly.
+<|toggle|theme|>
+
+<|navbar|>
---
+++ src/pages/root.py
@@ -0,0 +1,8 @@
+##© 2024 Tushar Aggarwal. All rights reserved.(https://tushar-aggarwal.com)
+##Taipy Cheat Sheets [https://github.com/tushar2704/Taipy-Cheat-Sheets] (https://github.com/tushar2704/Taipy-Cheat-Sheets)
+#######################################################################################################
+#Root Pages
+#######################################################################################################
+from taipy import Gui
+from taipy.gui import Markdown
+root_md = Markdown("src/pages/getting_started.md")
